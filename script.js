0be3df431bf03b60c8b529a57be263e96c165347
li3rd <li3rds@gmail.com>
2022-09-26 12:56:22 +0300

    промежуточная версия без стилизации select и option

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const amount = document.querySelector('#amount');
-const guests = document.querySelector('#guests');
-const percentage = document.querySelector('#percentage');
-const tipAmount = document.querySelector('#tip-amount');
+const amount = document.getElementById('amount');
+const guests = document.getElementById('guests');
+const percentage = document.getElementById('percentage');
+const tipAmount = document.getElementById('tip-amount');
 function calculate() {
     const tip = ((amount.value *percentage.value)/guests.value).toFixed();
     amount.value="";
@@ -9,8 +9,15 @@ function calculate() {
     guests.value="";
     if (tip === "NaN") {
         tipAmount.textContent = "Tip 0";
+        showTipAmount();
     }
         else{
             tipAmount.textContent = "Tip "+ tip;
+            showTipAmount();
         }
     }
+    showTipAmount = () => {
+        var x = document.getElementById("tip-amount");
+        x.className = "show";
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+      }
